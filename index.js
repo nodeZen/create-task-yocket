34acbb71f3e70183a6475ed4d68d0d0d040417bd
Surya Mokka <surya.reddy423@gmail.com>
2021-11-27 21:30:55 +0530

    Added static file path for build

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const cors = require("cors");
 require("dotenv").config();
 const user = require("./routes/user");
 const tasks = require("./routes/tasks");
+const path =  require("path");
 
 app.use(express.json());
 app.use(cors());
 app.use("/user",user);
 app.use("/tasks",tasks);
+
+
+app.use(express.static(path.join(__dirname, "client/build/")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client/build/index.html"));
+});
+
 app.listen(port,()=>{
     mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("Connected");
     })
-})
+});
